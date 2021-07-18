36100be47a2ce49822a67e132ae76583814c9385
De Dannie <dedannie@Des-Air.local>
2021-07-18 11:24:18 +0700

    Initialized

---
+++ index.js
@@ -0,0 +1,77 @@
+// Processing text input to get result
+function getAnswer() {
+  personAgeA = document.getElementById('age_of_death_a').value;
+  personYearA = document.getElementById('year_of_death_a').value;
+  personAgeB = document.getElementById('age_of_death_b').value;
+  personYearB = document.getElementById('year_of_death_b').value;
+
+  let personA = new Person(personAgeA, personYearA)
+  let personB = new Person(personAgeB, personYearB)
+  persons = [personA.killed(), personB.killed()]
+  average_killed = Average(persons)
+
+  answer1= document.getElementById('answer1')
+  answer2= document.getElementById('answer2')
+  answer3= document.getElementById('answer3')
+
+  substractionA = personYearA - personAgeA
+  substractionB = personYearB - personAgeB
+
+  answer1_value = 'Person A born on Year = ' + personA.born + ' - '+ personA.dead + ' = ' + substractionA + ', number of people killed on year ' + substractionA + ' is ' + personA.killed() + '.'
+  answer2_value = 'Person B born on Year = ' + personB.born + ' - '+ personB.dead + ' = ' + substractionB + ', number of people killed on year ' + substractionB + ' is ' + personB.killed() + '.'
+  answer3_value = 'So the average is ( ' + personB.killed() + ' + ' + personA.killed() + ')/' + persons.length + ' = ' + average_killed
+
+  answer1.innerHTML = answer1_value
+  answer2.innerHTML = answer2_value
+  answer3.innerHTML = answer3_value
+}
+
+// Check if number is prime
+function checkPrime(num) {
+  for (let i = 2; i * i <= num; i++)
+      if (num % i === 0)
+        return false; 
+  return num > 1;
+}
+
+// Get average from any numbers throw in
+function Average(numbers) {
+  var total = 0;
+  for(var i = 0; i < numbers.length; i++) {
+      total += numbers[i];
+  }
+  return total / numbers.length;
+}
+
+// Object of person information and how many killed he did at certain age and year
+function Person(age, year) {
+  this.born = age,
+  this.dead = year,
+  this.killed = function() {
+    numberofyears = year - age;
+    numberofkilled = 0;
+    i = 0;
+    while (i < numberofyears) {
+      if (i > 1) {
+        
+        j = i;
+        while (true) {
+          isPrime = checkPrime(j)
+          if (isPrime) {
+            numberofkilled += j;
+            break;
+          } else {
+            j++;
+          }
+        }
+        
+      } else {
+        numberofkilled += i;
+      }
+      i++;
+    }
+    numberofkilled += 1;
+    return numberofkilled;
+  };
+}
+module.exports = { checkPrime, Average, Person };
---
+++ index.test.js
@@ -0,0 +1,23 @@
+const { expect, test } = require('@jest/globals')
+const { checkPrime, Average, Person } = require('./index')
+
+// Initialize variable before test
+const personA = new Person(10, 12)
+const personB = new Person(13, 17)
+
+// Ensure that number killed has correct answer
+test('person info and people killed', () => {
+  expect(personA.killed()).toEqual(2)
+  expect(personB.killed()).toEqual(7)
+})
+
+// Ensure average number killed has correct answer
+test('average killing', () => {
+  const arrays = [personA.killed(), personB.killed()]
+  expect(Average(arrays)).toEqual(4.5)
+})
+
+// Ensure every number throw in can be detected as prime or not
+test('check if number is prime', () => {
+  expect(checkPrime(1)).toEqual(false)
+})
