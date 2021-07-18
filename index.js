0d527353ba40cb0920b4006e5dec3b3cfd1f9945
De Dannie <dedannie@Des-Air.local>
2021-07-18 16:56:44 +0700

    Witch year start from 1, otherwise 0

--- index.js
+++ index.js
@@ -5,33 +5,26 @@ function getAnswer() {
   personAgeB = document.getElementById('age_of_death_b').value;
   personYearB = document.getElementById('year_of_death_b').value;
 
-  if ((personYearA - personAgeA) > 0 && (personYearB - personAgeB) > 0) {
-    let personA = new Person(personAgeA, personYearA)
-    let personB = new Person(personAgeB, personYearB)
-    persons = [personA.killed(), personB.killed()]
-    average_killed = Average(persons)
-  
-    answer1= document.getElementById('answer1')
-    answer2= document.getElementById('answer2')
-    answer3= document.getElementById('answer3')
-  
-    substractionA = personYearA - personAgeA
-    substractionB = personYearB - personAgeB
-  
-    answer1_value = 'Person A born on Year = ' + personA.year + ' - '+ personA.age + ' = ' + substractionA + ', number of people killed on year ' + substractionA + ' is ' + personA.killed() + '.'
-    answer2_value = 'Person B born on Year = ' + personB.year + ' - '+ personB.age + ' = ' + substractionB + ', number of people killed on year ' + substractionB + ' is ' + personB.killed() + '.'
-    answer3_value = 'So the average is ( ' + personB.killed() + ' + ' + personA.killed() + ')/' + persons.length + ' = ' + average_killed
-  
-    answer1.innerHTML = answer1_value
-    answer2.innerHTML = answer2_value
-    answer3.innerHTML = answer3_value
-  } else {
-    answer1.innerHTML = ''
-    answer2.innerHTML = ''
-    answer3.innerHTML = '-1'
-  }
+  let personA = new Person(personAgeA, personYearA)
+  let personB = new Person(personAgeB, personYearB)
 
-  
+  substractionA = personA.year - personA.age
+  substractionB = personB.year - personB.age
+
+  persons = [personA.killed(), personB.killed()]
+  average_killed = Average(persons)
+
+  answer1= document.getElementById('answer1')
+  answer2= document.getElementById('answer2')
+  answer3= document.getElementById('answer3')
+
+  answer1_value = 'Person A born on Year = ' + personA.year + ' - '+ personA.age + ' = ' + substractionA + ', number of people killed on year ' + substractionA + ' is ' + personA.killed() + '.'
+  answer2_value = 'Person B born on Year = ' + personB.year + ' - '+ personB.age + ' = ' + substractionB + ', number of people killed on year ' + substractionB + ' is ' + personB.killed() + '.'
+  answer3_value = 'So the average is ( ' + personB.killed() + ' + ' + personA.killed() + ')/' + persons.length + ' = ' + average_killed
+
+  answer1.innerHTML = answer1_value
+  answer2.innerHTML = answer2_value
+  answer3.innerHTML = answer3_value
 }
 
 // Check if number is prime
@@ -53,33 +46,44 @@ function Average(numbers) {
 
 // Object of person information and how many killed did at certain age and year
 function Person(age, year) {
+  if (age < 0) {
+    age = -1;
+  }
+  if (year < 0) {
+    year = -1;
+  }
   this.age = age,
   this.year = year,
   this.killed = function() {
-    numberofyears = year - age;
-    numberofkilled = 0;
-    i = 0;
-    while (i < numberofyears) {
-      if (i > 1) {
-        
-        j = i;
-        while (true) {
-          isPrime = checkPrime(j)
-          if (isPrime) {
-            numberofkilled += j;
-            break;
-          } else {
-            j++;
+    numberofyears = this.year - this.age;
+    if (numberofyears > 0) {
+      numberofkilled = 0;
+      i = 0;
+      while (i < numberofyears) {
+        if (i > 1) {
+          
+          j = i;
+          while (true) {
+            isPrime = checkPrime(j)
+            if (isPrime) {
+              numberofkilled += j;
+              break;
+            } else {
+              j++;
+            }
           }
+          
+        } else {
+          numberofkilled += i;
         }
-        
-      } else {
-        numberofkilled += i;
+        i++;
       }
-      i++;
+      numberofkilled += 1;
+      return numberofkilled;
+    } else {
+      return 0
     }
-    numberofkilled += 1;
-    return numberofkilled;
+    
   };
 }
 
